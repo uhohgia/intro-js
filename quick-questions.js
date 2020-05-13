0054d055023390587271c2d620a331108217dbf7
Gianella Cortez <gianella.cortez@globant.com>
2020-05-12 21:06:35 -0700

    inline function expression

--- quick-questions.js
+++ quick-questions.js
@@ -120,7 +120,46 @@ helloCat(catSays);
 
 
 
+///////////////// Named Function expressions
 
+// Inline function expressions
+
+/*
+A function expression is when a function is assigned to a variable. 
+And, in JavaScript, this can also happen when you pass a function inline as an argument to another function.
+*/
+
+// Function expression that assigns the function displayFavorite 
+// to the variable favoriteMovie
+var favoriteMovie = function displayFavorite(movieName) {
+  console.log("My favorite movie is " + movieName);
+};
+
+// Function declaration that has two parameters: a function for displaying
+// a message, along with a name of a movie
+function movies(messageFunction, name) {
+  messageFunction(name);
+}
+
+// Call the movies function, pass in the favoriteMovie function and name of movie
+movies(favoriteMovie, "Finding Nemo");
+
+
+/*
+But you could have bypassed the first assignment of the function, 
+by passing the function to the movies() function inline.
+*/
+
+// Function declaration that takes in two arguments: a function for displaying
+// a message, along with a name of a movie
+function movies(messageFunction, name) {
+  messageFunction(name);
+}
+
+// Call the movies function, pass in the function and name of movie
+movies(function displayFavorite(movieName) {
+  console.log("My favorite movie is " + movieName);
+}, "Finding Nemo");
 
 
 
