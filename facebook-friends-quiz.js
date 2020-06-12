d2ac4f3f274da58cfb467b38c4a507825c97b870
Gianella Cortez <gianella.cortez@globant.com>
2020-06-11 22:03:31 -0700

    udacity answer to review

--- facebook-friends-quiz.js
+++ facebook-friends-quiz.js
@@ -72,4 +72,32 @@ facebookProfile.removeFriend();
 
 
 
+/*
 
+UDACITY ANSWER
+
+var facebookProfile = {
+    name: "Udacian",
+    friends: 25,
+    messages: ["Message 1", "Message 2", "Message 3", "Message 4"],
+    postMessage: function(message){
+        facebookProfile.messages.push(message);
+    },
+    deleteMessage: function(index){
+        // In the following splice() method call,
+        // - argument 1 = index from where the element has to be deleted
+        // - argument 2 = count of elements to be deleted
+        facebookProfile.messages.splice(index, 1);
+    },
+    addFriend: function(){
+        facebookProfile.friends = facebookProfile.friends + 1;
+        //friends += 1; // this statement is equivalent to the above
+    },
+    removeFriend: function(){
+        if(facebookProfile.friends>0)
+            facebookProfile.friends = facebookProfile.friends - 1;
+    }
+};
+
+
+*/
