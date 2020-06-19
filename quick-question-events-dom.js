52965c093b12dc256debff5b8ba259d2ddd3eea0
Gianella Cortez <gianella.cortez@globant.com>
2020-06-18 19:02:38 -0700

    event-node-element questions

--- quick-question-events-dom.js
+++ quick-question-events-dom.js
@@ -7,6 +7,21 @@ EventTarget Interface - top of the chain, does not inherit properties or methods
 Node Interface - inherits properties and methods from event target
 Element interface - inherits properties and methods from node
 
+*/
 
+
+/*
+Think about these interfaces:
+
+EventTarget
+Node
+Element
+Is there a difference between these two:
+
+document.addEventListener()
+myHeading.addEventListener() (assume the myHeading variable is an element)
+
+
+My answer: "myHeading is an element instead of a node like document"
 
 */
