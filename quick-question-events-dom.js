352374ad0def31f0615af5c9ed483acdba5c48b9
Gianella Cortez <gianella.cortez@globant.com>
2020-06-18 19:49:14 -0700

    event phases questions

--- quick-question-events-dom.js
+++ quick-question-events-dom.js
@@ -25,3 +25,37 @@ myHeading.addEventListener() (assume the myHeading variable is an element)
 My answer: "myHeading is an element instead of a node like document"
 
 */
+
+
+//////// PREGUNTA 2
+
+// No entiendo las fases y como funcionan.
+
+// Event Phases
+
+/*
+There are three different phases during the lifecycle of an event. They are:
+
+the capturing phase
+the at target phase
+and the bubbling phase
+
+They work in that order.
+
+*/
+
+document.addEventListener('click', function () {
+   console.log('The document was clicked');
+});
+
+// The document was clicked
+// Udacity answer : the body was clicked
+
+// Porque aca el console log se activa en el body y no en el documento?
+
+document.body.addEventListener('click', function () {
+    console.log('The body element was clicked');
+});
+
+// the body was clicked
+// Udacity answer : the document was clicked
