93604314e0295f20f1c23c75a837567cbaa8f431
Gianella Cortez <gianella.cortez@globant.com>
2020-06-18 18:35:10 -0700

    preguntas dom

---
+++ quick-question-events-dom.js
@@ -0,0 +1,12 @@
+/////// PREGUNTA 1
+
+/*
+Concepto de:
+
+EventTarget Interface - top of the chain, does not inherit properties or methods
+Node Interface - inherits properties and methods from event target
+Element interface - inherits properties and methods from node
+
+
+
+*/
